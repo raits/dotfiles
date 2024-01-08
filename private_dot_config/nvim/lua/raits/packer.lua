d21f545dda0643dd2538eab4580646b15efed356
Raido Kaju <raido.kaju@gmail.com>
2024-01-08 11:29:08 +0200

    chore: bumb telescope to v0.1.5

--- private_dot_config/nvim/lua/raits/packer.lua
+++ private_dot_config/nvim/lua/raits/packer.lua
@@ -65,7 +65,7 @@ return require('packer').startup(function(use)
   use('tpope/vim-fugitive')
   
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
   use('nvim-tree/nvim-tree.lua')
