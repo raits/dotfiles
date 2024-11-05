e29c6ff38e1d89663bf4f122dc51ee1f7027a42f
Raido Kaju <raido.kaju@gmail.com>
2024-11-05 20:07:22 +0200

    chore: ignore mkdir error

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ cd ~/.local
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
-mkdir ~/.local/bin
+mkdir -p ~/.local/bin
 ln -s ~/.local/squashfs-root/AppRun ~/.local/bin/nvim
 
 echo >&2 "====================================================================="
