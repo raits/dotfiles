1dd0be02ed10a0948af58e8d623f6aeaa6ec37ca
Raido Kaju <raido.kaju@niis.org>
2024-12-09 14:47:56 +0200

    chore: add conform rules for typescript and enable hybrid mode for vue

--- private_dot_config/nvim/lua/plugins/typescript-tools.lua
+++ private_dot_config/nvim/lua/plugins/typescript-tools.lua
@@ -8,7 +8,7 @@ return {
 				filetypes = {
 					"javascript",
 					"typescript",
-					-- "vue",
+					"vue",
 				},
 				settings = {
 					capabilities = require("cmp_nvim_lsp").default_capabilities(),
