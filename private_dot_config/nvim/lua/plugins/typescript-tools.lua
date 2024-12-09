d9a62bc2bb19c5e01ba8b38ab2760783d67bccc7
Raido Kaju <raido.kaju@niis.org>
2024-12-09 14:20:57 +0200

    chore: use typescript-tools for typescript, still no working Volar hybrid mode

---
+++ private_dot_config/nvim/lua/plugins/typescript-tools.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig", "hrsh7th/cmp-nvim-lsp" },
+		-- opts = {},
+		opts = function()
+			return {
+				filetypes = {
+					"javascript",
+					"typescript",
+					-- "vue",
+				},
+				settings = {
+					capabilities = require("cmp_nvim_lsp").default_capabilities(),
+					-- Might be required for Deno projects
+					-- root_dir = require("lspconfig").util.root_pattern_exclude({
+					-- 	root = { "package.json" },
+					-- 	exclude = { "deno.json", "deno.jsonc" },
+					-- }),
+					single_file_support = false,
+					tsserver_plugins = {
+						"@vue/typescript-plugin",
+					},
+				},
+			}
+		end,
+	},
+}
