d8411fa705005e13c8c95e05f38ddf761ef94d82
Raido Kaju <raido.kaju@niis.org>
2025-03-06 09:44:47 +0200

    feat: improve codecompanion setup

--- private_dot_config/nvim/lua/plugins/codecompanion.lua
+++ private_dot_config/nvim/lua/plugins/codecompanion.lua
@@ -4,5 +4,16 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
+		"j-hui/fidget.nvim",
+	},
+	-- Initialize the code companion fidget
+	init = function()
+		require("utilities.codecompanion-fidget"):init()
+	end,
+	-- Key mappings for Code Companion plugin
+	keys = {
+		{ "<leader>ac", "<cmd>CodeCompanionChat<CR>", desc = "Code Companion Chat" },
+		{ "<leader>aa", "<cmd>CodeCompanionActions<CR>", mode = { "n", "v" }, desc = " Code Companion Actions" },
+		{ "<leader>ap", "<cmd>CodeCompanion<CR>", mode = { "n", "v" }, desc = " Code Companion Prompt" },
 	},
 }
