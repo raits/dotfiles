8eaff65e0ca7c58586e3201753f01e9853e9718e
Raido Kaju <raido.kaju@niis.org>
2025-03-27 09:20:53 +0200

    chore: incorporate changes from dam9000/kickstart-modular.nvim

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -3,15 +3,15 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 --
 -- Leader to space
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
 
-require 'options'
+require("options")
 
-require 'keymaps'
+require("keymaps")
 
-require 'lazy-bootstrap'
+require("lazy-bootstrap")
 
-require 'lazy-plugins'
+require("lazy-plugins")
