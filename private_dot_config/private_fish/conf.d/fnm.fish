0e1d11edd8428b7d3b6222c3a3676a0fe42bbe5e
Raido Kaju <raido.kaju@niis.org>
2025-09-10 11:24:00 +0300

    chore: fix fnm check on fish startup

--- private_dot_config/private_fish/conf.d/fnm.fish
+++ private_dot_config/private_fish/conf.d/fnm.fish
@@ -1,1 +1,7 @@
-fnm env --use-on-cd --shell fish | source
+if test -x ~/.cargo/bin/fnm
+    ~/.cargo/bin/fnm env --use-on-cd --resolve-engines --shell fish | source
+else if command -v fnm >/dev/null 2>&1
+    fnm env --use-on-cd --shell fish | source
+else
+    echo "Warning: fnm is not available on this system" >&2
+end
